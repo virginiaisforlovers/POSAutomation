95940fbfadcec7c86c123baab087de3be617ba4d
selametle <selametle@mail.com>
2018-08-04 17:47:55 -0500

    representative name added

--- src/main/java/com/inss/atmn/RepresentativesTest.java
+++ src/main/java/com/inss/atmn/RepresentativesTest.java
@@ -15,9 +15,11 @@ public class RepresentativesTest extends RepresentativesTestBase {
   @Test
   public void testReps(){
     driver.get("https://www.house.gov/representatives");
-    representativesPage.getDistrictsByState("California");
-    representativesPage.getRepresentativeNameByState("California");
-    representativesPage.getPartyByState("California");
+
+    System.out.println(representativesPage.getDistrictsByState("California"));
+
+    //representativesPage.getRepresentativeNameByState("California");
+    //representativesPage.getPartyByState("California");
   }
 
 }
